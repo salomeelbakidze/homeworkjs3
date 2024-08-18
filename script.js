9bf2453fa49021c53dcfded0a64edd71e6d27be8
salomeelbakidze <elbakidzesalome23@gmail.com>
2024-08-18 18:54:49 +0400

    js

---
+++ script.js
@@ -0,0 +1,65 @@
+'use strict'
+
+// ..task1
+
+function 
+
+
+
+
+// ..task2
+
+let user = {
+    firstname: 'giorgi',
+    lastname: 'saakadze',
+    age: 32,
+    isloggedin: true
+  }
+
+  function getusername (user) {
+    if (user.isloggedin) {
+        return true;
+    }
+    else {
+        return false ;
+    }
+  }
+      
+  
+
+
+// ..task3
+
+// ..task4
+
+function getNumber (number) {
+    if (number % 2 === 0) {
+        return 'this number is even';
+    } else {
+        return 'this number is odd';
+    }
+}
+
+
+
+
+// ..task5
+
+let array = [1,2,3,4,5]; 
+
+for (let i = array.length - 1; i >= 0; i--) {
+    console.log(array[i]);
+}
+
+
+
+// ..task6
+
+
+
+
+
+// ..task7
+let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// ..task8
